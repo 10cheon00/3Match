b1b0512d9b0fb9f2b6ff4df221d44e1681ddc5cd
10cheon00 <10cheon00@gmail.com>
2023-08-12 14:45:24 +0900

    refactor: move swapping tiles code into new method

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -5,8 +5,7 @@ using UnityEngine.EventSystems;
 
 /*
 Tile의 역할
-1. 자료구조로부터 현재 타일에 어떤 블록이 담겨있는지 갖고와 보여준다.
-2. 사용자의 입력을 받아 어떤 타일과 교환되는지 확인 후 교환한다.
+1. 생성된 타일의 색을 관리한다.
 */
 
 public class Tile : MonoBehaviour
--- Assets/Scripts/TileBoardManager.cs
+++ Assets/Scripts/TileBoardManager.cs
@@ -19,19 +19,16 @@ public class TileBoardManager : MonoBehaviour
 
     public void SwapTile(Tile src, Tile dest)
     {
-        (int srcX, int srcY) = GetTileIndex(src);
-        (int destX, int destY) = GetTileIndex(src);
+        (int srcX, int srcY) = GetTileIndexes(src);
+        (int destX, int destY) = GetTileIndexes(src);
 
         Debug.Assert(srcX != -1 && srcY != -1 && destX != -1 && destY != -1);
 
-        (_tileBoard[destY][destX], _tileBoard[srcY][srcX]) = (
-            _tileBoard[srcY][srcX],
-            _tileBoard[destY][destX]
-        );
-        SwapTilePosition(src, dest);
+        SwapTileInTileBoard(srcX, srcY, destX, destY);
+        SwapTileObjectPosition(src, dest);
     }
 
-    private (int, int) GetTileIndex(Tile tile)
+    private (int, int) GetTileIndexes(Tile tile)
     {
         for (int i = 0; i < _tileBoard.Count; i++)
         {
@@ -46,7 +43,15 @@ public class TileBoardManager : MonoBehaviour
         return (-1, -1);
     }
 
-    private void SwapTilePosition(Tile src, Tile dest)
+    private void SwapTileInTileBoard(int srcX, int srcY, int destX, int destY)
+    {
+        (_tileBoard[destY][destX], _tileBoard[srcY][srcX]) = (
+            _tileBoard[srcY][srcX],
+            _tileBoard[destY][destX]
+        );
+    }
+
+    private void SwapTileObjectPosition(Tile src, Tile dest)
     {
         (dest.transform.localPosition, src.transform.localPosition) = (
             src.transform.localPosition,

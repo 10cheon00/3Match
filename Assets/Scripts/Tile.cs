dd9913524daa1e797fca1f2eb46e36d506b158bc
10cheon00 <10cheon00@gmail.com>
2023-08-12 15:34:26 +0900

    feat: create swapping effect

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -10,6 +10,8 @@ Tile의 역할
 
 public class Tile : MonoBehaviour
 {
+    private SpriteRenderer _spriteRenderer;
+
     [SerializeField]
     private TileColor _tileColor;
     public TileColor Color
@@ -17,4 +19,56 @@ public class Tile : MonoBehaviour
         get { return _tileColor; }
         set { _tileColor = value; }
     }
+
+    private Vector3 _rotationPoint;
+    private bool _canRotate = false;
+    private float _rotatedAngle = 0f;
+    private readonly float _angle = 15f;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_canRotate)
+        {
+            Rotate();
+
+            if (_rotatedAngle >= 180f)
+            {
+                End180DegreeRotation();
+            }
+        }
+    }
+    private void Rotate()
+    {
+        transform.RotateAround(_rotationPoint, Vector3.forward, _angle);
+        _rotatedAngle += _angle;
+
+        ResetLocalRotation();
+    }
+
+    private void ResetLocalRotation()
+    {
+        Quaternion quaternion = transform.rotation;
+        quaternion.eulerAngles = Vector3.zero;
+        transform.rotation = quaternion;
+    }
+
+    private void End180DegreeRotation()
+    {
+        _canRotate = false;
+        _spriteRenderer.sortingOrder = (int)TileBoardSortingOrder.Default;
+    }
+
+    public void Start180DegreeRotation(Vector3 rotationPoint)
+    {
+        _rotationPoint = rotationPoint;
+        _rotatedAngle = 0;
+        _canRotate = true;
+        _spriteRenderer.sortingOrder = (int)TileBoardSortingOrder.SwappingTile;
+    }
+
 }
--- Assets/Scripts/TileBoardManager.cs
+++ Assets/Scripts/TileBoardManager.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Drawing;
 using UnityEngine;
 
 /*
@@ -20,12 +17,15 @@ public class TileBoardManager : MonoBehaviour
     public void SwapTile(Tile src, Tile dest)
     {
         (int srcX, int srcY) = GetTileIndexes(src);
-        (int destX, int destY) = GetTileIndexes(src);
+        (int destX, int destY) = GetTileIndexes(dest);
 
         Debug.Assert(srcX != -1 && srcY != -1 && destX != -1 && destY != -1);
 
-        SwapTileInTileBoard(srcX, srcY, destX, destY);
-        SwapTileObjectPosition(src, dest);
+        if(AreTwoTilesAdjacent(srcX, srcY, destX, destY))
+        {
+            SwapTileInTileBoard(srcX, srcY, destX, destY);
+            PlayTileSwapEffect(src, dest);
+        }
     }
 
     private (int, int) GetTileIndexes(Tile tile)
@@ -43,12 +43,26 @@ public class TileBoardManager : MonoBehaviour
         return (-1, -1);
     }
 
+    private bool AreTwoTilesAdjacent(int srcX, int srcY, int destX, int destY)
+    {
+        int xDiff = Mathf.Abs(srcX - destX);
+        int yDiff = Mathf.Abs(srcY - destY);
+        return xDiff + yDiff == 1;
+    }
+
     private void SwapTileInTileBoard(int srcX, int srcY, int destX, int destY)
     {
         (_tileBoard[destY][destX], _tileBoard[srcY][srcX]) = (
             _tileBoard[srcY][srcX],
             _tileBoard[destY][destX]
         );
+    }
+
+    private void PlayTileSwapEffect(Tile src, Tile dest)
+    {
+        Vector3 midPoint = Vector3.Lerp(src.transform.position, dest.transform.position, 0.5f);
+        src.Start180DegreeRotation(midPoint);
+        dest.Start180DegreeRotation(midPoint);
     }
 
     private void SwapTileObjectPosition(Tile src, Tile dest)
